58744932ba8f7eaf61fa359f222bf778e796b7ab
suki9 <suki.music2004@gmail.com>
2023-11-19 15:47:30 +0900

    change: css -> scss

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,5 +1,5 @@
 // css -------------------------------------------------///
-import './styles/main.css';
+import './styles/main.scss';
 import './assets/fontello/css/fontello.css';
 
 // TS Module -------------------------------------------///

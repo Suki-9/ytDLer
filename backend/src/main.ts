5347efe8ed82610fc94ee2f4cf1cb186d2d89ad5
suki9 <suki.music2004@gmail.com>
2023-11-19 15:46:09 +0900

    change '関数名変更'

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-async function bootstrap() {
+async function boot() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(7700);
 }
-bootstrap();
+boot();

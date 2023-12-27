d99b07ff89218605d947829e24ca2552d8e3e5a7
suki9 <suki.music2004@gmail.com>
2023-12-28 04:39:00 +0900

    change: 拡張子を指定出来るように

--- backend/src/APIs/downloaderApi.controller.ts
+++ backend/src/APIs/downloaderApi.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Res, Logger } from '@nestjs/common';
+import { Controller, Post, Body, Res } from '@nestjs/common';
 import { DownloaderApiService } from './downloaderApi.service';
 
 import { Response } from 'express';
@@ -6,31 +6,28 @@ import { Response } from 'express';
 @Controller('/api')
 export class DownloaderApiController {
   constructor(private readonly ytDownloader: DownloaderApiService) {}
-  private readonly logger = new Logger(DownloaderApiController.name);
+  // private readonly logger = new Logger(DownloaderApiController.name);
 
   @Post('youtube-dl')
   async Downloader(
     @Body()
-    body: {
-      url: string;
-      id: string;
-      options: Record<string, boolean | string>;
-    },
+    body: YtdlPayload,
     @Res() res: Response,
   ): Promise<void> {
     try {
-      res.status(200).json({
-        msg: 'DL Success!',
-        url: body.url,
-        ...(await this.ytDownloader.Downloader(
-          body.url,
-          body.id,
-          body.options,
-        )),
-      });
+      if (body.options.mimeType in this.ytDownloader.mimeType)
+        res.status(200).json({
+          msg: 'DL Success!',
+          url: body.url,
+          ...(await this.ytDownloader.Downloader(
+            body.url,
+            body.id,
+            body.options,
+          )),
+        });
     } catch (err) {
       console.error(err);
-      res.status(500).json({ msg: 'DL fail!!!' });
+      res.status(500).json({ err: 'DL fail!!!' });
     }
   }
 }
--- backend/src/APIs/downloaderApi.service.ts
+++ backend/src/APIs/downloaderApi.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { createWriteStream } from 'fs';
-import { exec } from 'child_process';
+import { exec, ExecException } from 'child_process';
 
 import ytdl = require('ytdl-core');
 
@@ -8,59 +8,76 @@ import ytdl = require('ytdl-core');
 export class DownloaderApiService {
   private readonly logger = new Logger(DownloaderApiService.name);
 
+  public mimeType = {
+    audio: ['audio/mp3', 'audio/wav', 'audio/flac', 'audio/ogg'],
+    video: ['video/mp4', 'video/avi'],
+  } as const;
+
+  private fileConvert = async (
+    before: string,
+    after: string,
+  ): Promise<boolean | ExecException> =>
+    new Promise<boolean | ExecException>((resolve, reject) =>
+      exec(`ffmpeg -i ${before} ${after}`, (err, stdout, stderr) =>
+        err
+          ? (this.logger.error(stderr), reject(err))
+          : (this.logger.log(stdout), resolve(true)),
+      ),
+    );
+
   Downloader = async (
     url: string,
     id: string,
-    options: Record<string, boolean | string>,
-  ): Promise<Record<string, string>> => {
-    return new Promise(async (resolve) => {
-      let downloadURL = `/DL/result_${id}.mp4`;
-      const info = await ytdl.getInfo(url);
-      const audio = ytdl(url, { quality: 'highestaudio' });
-      audio.pipe(createWriteStream(`./Public/DL/${id}.wav`));
+    options: YtdlPayload['options'],
+  ): Promise<YtdlResponse> =>
+    new Promise(async (resolve, reject) =>
+      ytdl(url, { quality: 'highestaudio' })
+        .pipe(createWriteStream(`./Public/DL/${id}.wav`))
+        .on('end', async () => {
+          const info = await ytdl.getInfo(url);
+          const downloadURL = `./Public/DL/result_${id}.${
+            options.mimeType.split('/')[1]
+          }`;
 
-      audio.on('end', async () => {
-        if (options.soundOnly) {
-          exec(
-            `ffmpeg -i ./Public/DL/${id}.wav -vn -ac 2 -ar 44100 -ab 256k -acodec libmp3lame -f mp3 ./Public/DL/result_${id}.mp3`,
-            (err, stdout, stderr) => {
-              if (err) this.logger.error(stderr);
-              else this.logger.log(stdout);
-              resolve({
-                url: downloadURL,
-                title: info.videoDetails.title,
-                uploadDate: info.videoDetails.uploadDate,
-                videoId: info.videoDetails.videoId,
-                viewCount: info.videoDetails.viewCount,
+          if (options.soundOnly && options.mimeType in this.mimeType) {
+            // デフォルトの wav 以外はコンバートする。
+            if (options.mimeType !== 'audio/wav')
+              try {
+                this.fileConvert(`./Public/DL/${id}.wav`, downloadURL);
+              } catch {
+                reject('convert failed!!');
+              }
+
+            resolve({
+              url: downloadURL,
+              title: info.videoDetails.title,
+              uploadDate: info.videoDetails.uploadDate,
+              videoId: info.videoDetails.videoId,
+              viewCount: info.videoDetails.viewCount,
+            });
+          } else {
+            ytdl(url, {
+              filter: (format) => format.container === 'mp4',
+            })
+              .pipe(createWriteStream(`./Public/DL/${id}.mp4`))
+              .on('end', () => {
+                exec(
+                  `ffmpeg -i ./Public/DL/${id}.mp4 -i ./Public/DL/${id}.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 ${downloadURL}`,
+                  (err, stdout, stderr) => {
+                    if (err) this.logger.error(stderr), reject(err);
+                    else
+                      this.logger.log(stdout),
+                        resolve({
+                          url: downloadURL,
+                          title: info.videoDetails.title,
+                          uploadDate: info.videoDetails.uploadDate,
+                          videoId: info.videoDetails.videoId,
+                          viewCount: info.videoDetails.viewCount,
+                        });
+                  },
+                );
               });
-            },
-          );
-          downloadURL = `/DL/result_${id}.mp3`;
-        } else {
-          const video = ytdl(url, {
-            filter: (format) => format.container === 'mp4',
-          });
-
-          video.pipe(createWriteStream(`./Public/DL/${id}.mp4`));
-
-          video.on('end', () => {
-            exec(
-              `ffmpeg -i ./Public/DL/${id}.mp4 -i ./Public/DL/${id}.wav -c:v copy -c:a aac -map 0:v:0 -map 1:a:0 ./Public/DL/result_${id}.mp4`,
-              (err, stdout, stderr) => {
-                if (err) this.logger.error(stderr);
-                else this.logger.log(stdout);
-                resolve({
-                  url: downloadURL,
-                  title: info.videoDetails.title,
-                  uploadDate: info.videoDetails.uploadDate,
-                  videoId: info.videoDetails.videoId,
-                  viewCount: info.videoDetails.viewCount,
-                });
-              },
-            );
-          });
-        }
-      });
-    });
-  };
+          }
+        }),
+    );
 }

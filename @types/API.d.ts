0ad491160b6615ecccc8241a119651deb0ae095b
suki-9 <suki.music2004@gmail.com>
2024-04-20 08:23:51 +0900

    move 'type declare files'

---
+++ @types/API.d.ts
@@ -0,0 +1,32 @@
+type MimeTypes = {
+  audio: 'audio/mp3' | 'audio/wav' | 'audio/flac' | 'audio/ogg';
+  video: 'video/mp4' | 'video/avi';
+};
+
+type Endpoints = {
+  'youtube-dl': {
+    req: {
+      id: string;
+      url: string;
+      options: {
+        silent: boolean;
+        mimeType: MimeTypes['audio'] | MimeTypes['video'];
+      };
+    };
+    res: {
+      url: string;
+      title: string;
+      uploadDate: string;
+      videoId: string;
+      viewCount: number;
+    };
+  };
+  'progress': {
+    req: {
+      id: string;
+    },
+    res: {
+      progress: number,
+    },
+  },
+};
